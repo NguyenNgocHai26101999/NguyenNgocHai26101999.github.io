1d30f4fbc72546b1a0e280782b3a2c0927e5bc90
NguyenNgocHai26101999 <129771410+NguyenNgocHai26101999@users.noreply.github.com>
2023-04-04 14:19:41 +0700

    Add files via upload

---
+++ flogin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyCoffee
+{
+    public partial class flogin : Form
+    {
+        public flogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            ftableManager f = new ftableManager();
+            this.Hide();
+            f.ShowDialog();
+            f.Show();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void flogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có thật sự muốn thoát chương trình", "Thông báo", MessageBoxButtons.OKCancel) != DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
